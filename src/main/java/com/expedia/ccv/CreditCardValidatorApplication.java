e24de9f4574588ccff999724b1bc1a7943fe7e4b
Akshay Pethani <patelaksh412@gmail.com>
2018-10-27 08:57:11 +0530

    Initial commit

---
+++ src/main/java/com/expedia/ccv/CreditCardValidatorApplication.java
@@ -0,0 +1,12 @@
+package com.expedia.ccv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardValidatorApplication.class, args);
+	}
+}
